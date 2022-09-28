bd3f69fefaf3702e2c2d599d8a876a48ec13e5df
Elson <elsonnezaj2005@gmail.com>
2022-09-28 17:13:54 +0200

    rolodex

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
-
 import { Card } from '../card/card.component.jsx'
 import './card-list.style.css'
 
 export const CardList = ({ monsters }) => {
   return (
     <div className="card-list">
-      {monsters.map((monster) => (
-        <Card key={monster.id} monster={monster}></Card>
-      ))}
+      {monsters.map((monster) => {
+        return <Card key={monster.id} monster={monster}></Card>
+      })}
     </div>
   )
 }
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import './card.style.css'
 
-export const Card = (props) => {
+export const Card = ({ monster }) => {
   return (
     <div className="card-container">
       <img
         alt="monster"
-        src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+        src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
       ></img>
-      <h2>{props.monster.name}</h2>
-      <p>{props.monster.email}</p>
+      <h2>{monster.name}</h2>
+      <p>{monster.email}</p>
+      <a className="link" href={monster.website}>
+        {monster.website}
+      </a>
     </div>
   )
 }
